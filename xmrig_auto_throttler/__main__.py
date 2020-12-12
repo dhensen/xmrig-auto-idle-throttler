cede0d23d55221383a3fe6a394eda22dfa80bbbc
Dino Hensen <dino.hensen@gmail.com>
2020-12-12 23:01:38 +0100

    chore: refactor

--- xmrig_auto_throttler/__main__.py
+++ xmrig_auto_throttler/__main__.py
@@ -1,87 +1,83 @@
 import json
 import sys
-import subprocess
+
 import time
 import logging
 
 from .version import __version__
 from .xmrig_api_client import XmrigClient
+from .idle import get_idle_time_sec
+from .dependencies import check_dependencies
+from .args import get_args
 
 logging.basicConfig(level=logging.INFO, format="%(asctime)s %(levelname)s %(message)s")
 
-RESOLUTION_SEC = 5
-MAXIMUM_PROFILE_TIMEOUT_SEC = 60
-MINIMUM_PROFILE_TIMEOUT_SEC = 10
-assert RESOLUTION_SEC < MINIMUM_PROFILE_TIMEOUT_SEC
 
-activated = False
-xmrig_http_client = XmrigClient("http://127.0.0.1:8810", "foobar")
 logger = logging.getLogger()
 
 
-def run(command, timeout=None):
-    return subprocess.run(
-        command,
-        shell=True,
-        stderr=subprocess.PIPE,
-        stdout=subprocess.PIPE,
-        universal_newlines=True,
-        timeout=timeout,
-    )
+class AutoThrottler:
+    def __init__(
+        self,
+        interval=5,
+        maximum_profile_timeout_sec=60,
+        minimum_profile_timeout_sec=10,
+        xmrig_host="http://127.0.0.1:8810",
+        xmrig_port="foobar",
+    ) -> None:
+        super().__init__()
+        assert interval < minimum_profile_timeout_sec
+        self.interval = interval
+        self.max_profile_timeout = maximum_profile_timeout_sec
+        self.min_profile_timeout = minimum_profile_timeout_sec
+        self.activated = False
+        self.xmrig_http_client = XmrigClient(xmrig_host, xmrig_port)
 
+    def is_active(self):
+        return self.activated
 
-def has_dependency(program: str):
-    res = run(f"which {program}")
-    return res.returncode == 0
+    def activate(self):
+        with open("maximum-config.json") as max_json_file:
+            max_config = json.load(max_json_file)
+            self.xmrig_http_client.set_config(max_config)
+        logger.info("activate")
+        self.activated = True
 
+    def deactivate(self):
+        with open("minimum-config.json") as min_json_file:
+            min_config = json.load(min_json_file)
+            self.xmrig_http_client.set_config(min_config)
+        logger.info("deactivate")
+        self.activated = False
 
-def get_idle_time_sec():
-    res = run("xprintidle")
-    idle_time_sec = int(res.stdout) / 1000
-    return idle_time_sec
-
-
-def is_active():
-    return activated
-
-
-def activate():
-    global activated, xmrig_http_client
-    with open("maximum-config.json") as max_json_file:
-        max_config = json.load(max_json_file)
-        xmrig_http_client.set_config(max_config)
-    logger.info("activate")
-    activated = True
-
-
-def deactivate():
-    global activated
-    with open("minimum-config.json") as min_json_file:
-        min_config = json.load(min_json_file)
-        xmrig_http_client.set_config(min_config)
-    logger.info("deactivate")
-    activated = False
-
-
-def main():
-    if sys.platform == "linux" and not has_dependency("xprintidle"):
-        logger.info("you have to install xprintidle first")
-        sys.exit(1)
-
-    try:
+    def run(self):
         while True:
             idle_time_sec = get_idle_time_sec()
             logger.info(f"idle for {idle_time_sec} seconds")
 
-            if idle_time_sec > MAXIMUM_PROFILE_TIMEOUT_SEC:
-                if not is_active():
-                    activate()
+            if idle_time_sec > self.max_profile_timeout:
+                if not self.is_active():
+                    self.activate()
 
-            if is_active():
-                if idle_time_sec < MINIMUM_PROFILE_TIMEOUT_SEC:
-                    deactivate()
+            if self.is_active():
+                if idle_time_sec < self.min_profile_timeout:
+                    self.deactivate()
 
-            time.sleep(RESOLUTION_SEC)
+            time.sleep(self.interval)
+
+
+def main():
+    check_dependencies()
+    args = get_args()
+    throttler = AutoThrottler(
+        interval=args.interval,
+        maximum_profile_timeout_sec=args.max_profile_timeout,
+        minimum_profile_timeout_sec=args.min_profile_timeout,
+        xmrig_host=args.xmrig_api_url,
+    )
+
+    try:
+        throttler.run()
     except KeyboardInterrupt:
         logger.info("program stopped by user")
 
